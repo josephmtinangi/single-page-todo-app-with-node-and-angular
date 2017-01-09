f709233def285342705f9c474387457e7446ed3f
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 22:22:03 +0300

    Get all todos

---
+++ public/core.js
@@ -0,0 +1,16 @@
+// public/core.js
+var mtinangiTodo = angular.module('mtinangiTodo', []);
+
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  // when landing on the page, get all todos and show them
+  $http.get('/api/todos')
+      .success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+}
