b37f3974b343c2a2063fd27239fcb0765b154e66
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 22:13:22 +0300

    Delete a todo

--- server.js
+++ server.js
@@ -60,7 +60,24 @@ app.post('/api/todos', function(req, res) {
       res.json(todos);
     });
   });
-  
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if(err)
+      res.send(err);
+
+    // get and return all the todos after you delete another
+    Todo.find(function(err, todos) {
+      if(err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
 });
 
 // listen (start app with node server.js) ============
