a4afe399908043c74e8d71da0b2c1595f543361a
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 22:10:35 +0300

    Create a todo

--- server.js
+++ server.js
@@ -42,6 +42,27 @@ app.get('/api/todos', function(req, res) {
   });
 });
 
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+  // create a todo, information comes from AJAX request from Angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if(err)
+      res.send(err);
+
+    // get and return all the todos after you create another
+    Todo.find(function(err, todos) {
+      if(err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+  
+});
+
 // listen (start app with node server.js) ============
 app.listen(8080);
 console.log("App listening on port 8080");
