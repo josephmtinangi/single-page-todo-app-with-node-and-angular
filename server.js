9a3be7dd200c057db52ada03f584b62b666ea5c4
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 22:17:58 +0300

    Define frontend route

--- server.js
+++ server.js
@@ -80,6 +80,11 @@ app.delete('/api/todos/:todo_id', function(req, res) {
   });
 });
 
+// application -------------
+app.get('*', function(req, res) {
+  res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 // listen (start app with node server.js) ============
 app.listen(8080);
 console.log("App listening on port 8080");
