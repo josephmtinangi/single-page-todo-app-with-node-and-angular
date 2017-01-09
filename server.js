59d6ea9c41b8f2ae6c3e9356886aee533409252e
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 21:51:35 +0300

    Configure

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+
+// set up =============================
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration =================
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(methodOverride());
