095368215427f872c86a6900ba3c26820f4a3fbe
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 21:53:32 +0300

    Listen

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
+
+// listen (start app with node server.js) ============
+app.listen(8080);
+console.log("App listening on port 8080");
